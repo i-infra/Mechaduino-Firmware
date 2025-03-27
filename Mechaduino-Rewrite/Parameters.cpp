5b3f28479d507883fc0a200bd8c175a92e3524ab
i-infra <77526490+i-infra@users.noreply.github.com>
2025-03-27 02:31:26 -0400

    drop partial rewrite

--- Mechaduino-Rewrite/Parameters.cpp
+++
@@ -1,3 +0,0 @@
-// Initializes the constants for Mechaduino
-
-const float Fs = TRIGGER_FREQ;  // Interrupt frequency in Hz
--- Mechaduino-Rewrite/Startup.h
+++
@@ -1,9 +0,0 @@
-// Contains declarations for setup functions used by firmware.
-
-#ifndef __STARTUP_H__
-#define __STARTUP_H__
-
-void setupPins();
-void setupSPI();
-
-#endif
